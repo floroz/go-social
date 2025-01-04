43bb8a14c6ef7b1c524fe5d2713a5a15bce8ca0d
Daniele Tortora <45793869+floroz@users.noreply.github.com>
2025-01-04 17:50:06 +0100

    feat: connect db in main with sql packages

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,16 +1,34 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"log"
 
 	"github.com/floroz/go-social/internal/env"
+	_ "github.com/lib/pq"
 )
 
+func connectDb() (*sql.DB, error) {
+	db, err := sql.Open("postgres", fmt.Sprintf("postgres://%s:%s@%s/%s?sslmode=disable", env.GetEnvValue("DB_USER"), env.GetEnvValue("DB_PASSWORD"), env.GetEnvValue("DB_HOST"), env.GetEnvValue("DB_NAME")))
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
 func main() {
-	env.MustLoadEnv()
+	env.MustLoadEnv(".env.local")
+
+	db, err := connectDb()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
 
 	config := &config{
-		port: env.GetEnvValue("ADDRESS", ":8080"),
+		port: env.GetEnvValue("PORT"),
 	}
 
 	app := &application{
--- go.mod
+++ go.mod
@@ -3,6 +3,11 @@ module github.com/floroz/go-social
 go 1.23.4
 
 require (
-	github.com/go-chi/chi/v5 v5.2.0 // indirect
-	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/go-chi/chi/v5 v5.2.0
+	github.com/joho/godotenv v1.5.1
+)
+
+require (
+	github.com/lib/pq v1.10.9 // indirect
+	golang.org/x/crypto v0.31.0 // indirect
 )
