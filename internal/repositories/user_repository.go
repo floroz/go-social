3875c124ac6d901a48a334c7a7254b9b92c14bfb
Daniele Tortora <45793869+floroz@users.noreply.github.com>
2025-01-05 11:10:45 +0100

    feat: impl scaffolding for interface methods with updated signatures

--- internal/repositories/user_repository.go
+++ internal/repositories/user_repository.go
@@ -18,7 +18,7 @@ func NewUserRepository(db *sql.DB) interfaces.UserRepository {
 	return &UserRepositoryImpl{db: db}
 }
 
-func (r *UserRepositoryImpl) Create(ctx context.Context, createUser *domain.CreateUserDTO) (*domain.User, error) {
+func (r *UserRepositoryImpl) Create(ctx context.Context, createUser *domain.UserDTO) (*domain.User, error) {
 	query := `
         INSERT INTO users (first_name, last_name, email, password)
         VALUES ($1, $2, $3, $4)
@@ -49,30 +49,31 @@ func (r *UserRepositoryImpl) Create(ctx context.Context, createUser *domain.Crea
 	return &user, nil
 }
 
-// func (r *UserRepositoryImpl) GetByID(ctx context.Context, id int) (*domain.User, error) {
-// 	query := `
-// 		SELECT id, first_name, last_name, email, password
-// 		FROM users
-// 		WHERE id = $1`
+func (r *UserRepositoryImpl) GetByID(ctx context.Context, id int) (*domain.User, error) {
+	return nil, nil
+	// 	query := `
+	// 		SELECT id, first_name, last_name, email, password
+	// 		FROM users
+	// 		WHERE id = $1`
 
-// 	user := &domain.User{}
-// 	err := r.db.QueryRowContext(ctx, query, id).Scan(
-// 		&user.ID,
-// 		&user.FirstName,
-// 		&user.LastName,
-// 		&user.Email,
-// 		&user.Password,
-// 	)
+	// 	user := &domain.User{}
+	// 	err := r.db.QueryRowContext(ctx, query, id).Scan(
+	// 		&user.ID,
+	// 		&user.FirstName,
+	// 		&user.LastName,
+	// 		&user.Email,
+	// 		&user.Password,
+	// 	)
 
-// 	if err != nil {
-// 		if errors.Is(err, sql.ErrNoRows) {
-// 			return nil, ErrNotFound
-// 		}
-// 		return nil, fmt.Errorf("get user by id: %w", err)
-// 	}
+	// 	if err != nil {
+	// 		if errors.Is(err, sql.ErrNoRows) {
+	// 			return nil, ErrNotFound
+	// 		}
+	// 		return nil, fmt.Errorf("get user by id: %w", err)
+	// 	}
 
-// 	return user, nil
-// }
+	// return user, nil
+}
 
 func (r *UserRepositoryImpl) GetByEmail(ctx context.Context, email string) (*domain.User, error) {
 	query := `
@@ -100,73 +101,75 @@ func (r *UserRepositoryImpl) GetByEmail(ctx context.Context, email string) (*dom
 	return user, nil
 }
 
-// func (r *UserRepositoryImpl) Update(ctx context.Context, updateUser *domain.UpdateUserDTO) (*domain.User, error) {
-// 	query := `
-// 		UPDATE users
-// 		SET first_name = $1, last_name = $2, email = $3, password = $4
-// 		WHERE id = $5
-// 		RETURNING id, first_name, last_name, email, password
-// 		`
+func (r *UserRepositoryImpl) Update(ctx context.Context, updateUser *domain.UserDTO) (*domain.User, error) {
+	return nil, nil
+	// 	query := `
+	// 		UPDATE users
+	// 		SET first_name = $1, last_name = $2, email = $3, password = $4
+	// 		WHERE id = $5
+	// 		RETURNING id, first_name, last_name, email, password
+	// 		`
 
-// 	user := domain.User{}
+	// 	user := domain.User{}
 
-// 	err := r.db.QueryRowContext(
-// 		ctx,
-// 		query,
-// 		user.FirstName,
-// 		user.LastName,
-// 		user.Email,
-// 		user.Password,
-// 		user.ID,
-// 	).Scan(
-// 		&user.ID,
-// 		&user.FirstName,
-// 		&user.LastName,
-// 		&user.Email,
-// 		&user.Password,
-// 	)
+	// 	err := r.db.QueryRowContext(
+	// 		ctx,
+	// 		query,
+	// 		user.FirstName,
+	// 		user.LastName,
+	// 		user.Email,
+	// 		user.Password,
+	// 		user.ID,
+	// 	).Scan(
+	// 		&user.ID,
+	// 		&user.FirstName,
+	// 		&user.LastName,
+	// 		&user.Email,
+	// 		&user.Password,
+	// 	)
 
-// 	if err != nil {
-// 		if errors.Is(err, sql.ErrNoRows) {
-// 			return nil, ErrNotFound
-// 		}
-// 		return nil, fmt.Errorf("update user: %w", err)
-// 	}
+	// 	if err != nil {
+	// 		if errors.Is(err, sql.ErrNoRows) {
+	// 			return nil, ErrNotFound
+	// 		}
+	// 		return nil, fmt.Errorf("update user: %w", err)
+	// 	}
 
-// 	return &user, nil
-// }
+	// return &user, nil
+}
 
-// func (r *UserRepositoryImpl) Delete(ctx context.Context, id int) error {
-// 	return nil
-// }
+func (r *UserRepositoryImpl) Delete(ctx context.Context, id int) error {
+	return nil
+}
 
-// func (r *UserRepositoryImpl) List(ctx context.Context, limit, offset int) ([]domain.User, error) {
-// 	query := `
-// 		SELECT id, first_name, last_name, email, password
-// 		FROM users
-// 		LIMIT $1 OFFSET $2`
+func (r *UserRepositoryImpl) List(ctx context.Context, limit, offset int) ([]domain.User, error) {
+	return nil, nil
+	// 	query := `
+	// 		SELECT id, first_name, last_name, email, password
+	// 		FROM users
+	// 		LIMIT $1 OFFSET $2`
 
-// 	rows, err := r.db.QueryContext(ctx, query, limit, offset)
-// 	if err != nil {
-// 		return nil, fmt.Errorf("list users: %w", err)
-// 	}
-// 	defer rows.Close()
+	// 	rows, err := r.db.QueryContext(ctx, query, limit, offset)
+	// 	if err != nil {
+	// 		return nil, fmt.Errorf("list users: %w", err)
+	// 	}
+	// 	defer rows.Close()
 
-// 	users := []domain.User{}
-// 	for rows.Next() {
-// 		user := domain.User{}
-// 		err := rows.Scan(
-// 			&user.ID,
-// 			&user.FirstName,
-// 			&user.LastName,
-// 			&user.Email,
-// 			&user.Password,
-// 		)
-// 		if err != nil {
-// 			return nil, fmt.Errorf("list users: %w", err)
-// 		}
-// 		users = append(users, user)
-// 	}
+	// 	users := []domain.User{}
+	// 	for rows.Next() {
+	// 		user := domain.User{}
+	// 		err := rows.Scan(
+	// 			&user.ID,
+	// 			&user.FirstName,
+	// 			&user.LastName,
+	// 			&user.Email,
+	// 			&user.Password,
+	// 		)
+	// 		if err != nil {
+	// 			return nil, fmt.Errorf("list users: %w", err)
+	// 		}
+	// 		users = append(users, user)
+	// 	}
 
-// 	return users, nil
-// }
+	// return users, nil
+}
--- internal/services/user_service.go
+++ internal/services/user_service.go
@@ -2,15 +2,13 @@ package services
 
 import (
 	"context"
-	"regexp"
 
 	"golang.org/x/crypto/bcrypt"
 
 	"github.com/floroz/go-social/internal/domain"
 	"github.com/floroz/go-social/internal/interfaces"
+	"github.com/floroz/go-social/internal/validation"
 )
-
-var emailRegex = regexp.MustCompile(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`)
 
 type userService struct {
 	userRepo interfaces.UserRepository
@@ -22,32 +20,16 @@ func NewUserService(userRepo interfaces.UserRepository) interfaces.UserService {
 	}
 }
 
-func (s *userService) CreateUser(ctx context.Context, user *domain.CreateUserDTO) (*domain.User, error) {
-	// validation
-	switch {
-	case user.FirstName == "":
-		return nil, domain.NewValidationError("first_name", "first name is required")
-	case user.LastName == "":
-		return nil, domain.NewValidationError("last_name", "last name is required")
-	case user.Email == "":
-		return nil, domain.NewValidationError("email", "email is required")
-	case !emailRegex.MatchString(user.Email):
-		return nil, domain.NewValidationError("email", "invalid email format")
-	case user.Password == "":
-		return nil, domain.NewValidationError("password", "password is required")
-	case len(user.Password) < 8:
-		return nil, domain.NewValidationError("password", "password must be at least 8 characters")
+func (s *userService) Create(ctx context.Context, user *domain.UserDTO) (*domain.User, error) {
+	err := validation.ValidateUserDTO(user)
+	if err != nil {
+		return nil, err
 	}
 
-	// check that user with email does not exist
-	existing, err := s.userRepo.GetByEmail(ctx, user.Email)
-
-	if existing != nil {
+	if existingUser, err := s.userRepo.GetByEmail(ctx, user.Email); existingUser != nil {
 		// obfuscate error message to avoid leaking user information
 		return nil, domain.NewBadRequestError("invalid body request")
-	}
-
-	if err != nil && err != domain.ErrNotFound {
+	} else if err != nil && err != domain.ErrNotFound {
 		return nil, domain.NewInternalServerError("something went wrong")
 	}
 
@@ -69,25 +51,19 @@ func (s *userService) CreateUser(ctx context.Context, user *domain.CreateUserDTO
 	return createdUser, nil
 }
 
-// func (s *userService) GetUserByID(ctx context.Context, id int) (*domain.User, error) {
-// 	return s.userRepo.GetByID(ctx, id)
-// }
+func (s *userService) GetByID(ctx context.Context, id int) (*domain.User, error) {
+	return s.userRepo.GetByID(ctx, id)
+}
 
-// func (s *userService) UpdateUser(ctx context.Context, user *domain.UpdateUserDTO) (*domain.UpdateUserDTO, error) {
-// 	if user.Password != "" {
-// 		hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
-// 		if err != nil {
-// 			return nil, fmt.Errorf("failed to hash password: %w", err)
-// 		}
-// 		user.Password = string(hashedPassword)
-// 	}
-// 	return s.userRepo.Update(ctx, user), nil
-// }
+func (s *userService) Update(ctx context.Context, user *domain.UserDTO) (*domain.User, error) {
+	updatedUser, err := s.userRepo.Update(ctx, user)
+	return updatedUser, err
+}
 
-// func (s *userService) DeleteUser(ctx context.Context, id int) error {
-// 	return s.userRepo.Delete(ctx, id)
-// }
+func (s *userService) Delete(ctx context.Context, id int) error {
+	return s.userRepo.Delete(ctx, id)
+}
 
-// func (s *userService) ListUsers(ctx context.Context, limit, offset int) ([]domain.User, error) {
-// 	return s.userRepo.List(ctx, limit, offset)
-// }
+func (s *userService) List(ctx context.Context, limit, offset int) ([]domain.User, error) {
+	return s.userRepo.List(ctx, limit, offset)
+}
