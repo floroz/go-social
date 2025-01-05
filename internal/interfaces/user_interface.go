6b63085f2ff11d18756675b160cc6caeed01bc85
Daniele Tortora <45793869+floroz@users.noreply.github.com>
2025-01-05 11:10:16 +0100

    feat: enable remaining interface methods

--- internal/interfaces/user_interface.go
+++ internal/interfaces/user_interface.go
@@ -7,18 +7,18 @@ import (
 )
 
 type UserRepository interface {
-	Create(ctx context.Context, createUser *domain.CreateUserDTO) (*domain.User, error)
+	Create(ctx context.Context, createUser *domain.UserDTO) (*domain.User, error)
 	GetByEmail(ctx context.Context, email string) (*domain.User, error)
-	// Update(ctx context.Context, updateUser *domain.UpdateUserDTO) (*domain.User, error)
-	// GetByID(ctx context.Context, id int) (*domain.User, error)
-	// Delete(ctx context.Context, id int) error
-	// List(ctx context.Context, limit, offset int) ([]domain.User, error)
+	Update(ctx context.Context, updateUser *domain.UserDTO) (*domain.User, error)
+	GetByID(ctx context.Context, id int) (*domain.User, error)
+	Delete(ctx context.Context, id int) error
+	List(ctx context.Context, limit, offset int) ([]domain.User, error)
 }
 
 type UserService interface {
-	CreateUser(ctx context.Context, createUser *domain.CreateUserDTO) (*domain.User, error)
-	// GetUserByID(ctx context.Context, id int) (*domain.User, error)
-	// UpdateUser(ctx context.Context, updateUser *domain.UpdateUserDTO) (*domain.User, error)
-	// DeleteUser(ctx context.Context, id int) error
-	// ListUsers(ctx context.Context, limit, offset int) ([]domain.User, error)
+	Create(ctx context.Context, createUser *domain.UserDTO) (*domain.User, error)
+	Update(ctx context.Context, updateUser *domain.UserDTO) (*domain.User, error)
+	GetByID(ctx context.Context, id int) (*domain.User, error)
+	Delete(ctx context.Context, id int) error
+	List(ctx context.Context, limit, offset int) ([]domain.User, error)
 }
