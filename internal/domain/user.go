cd2500ed80aaccc1e2865bd383dd93ae931a9f06
Daniele Tortora <45793869+floroz@users.noreply.github.com>
2025-01-05 11:10:05 +0100

    refactor: unify domain for dto

--- internal/domain/user.go
+++ internal/domain/user.go
@@ -10,17 +10,9 @@ type User struct {
 	UpdatedAt string
 }
 
-type CreateUserDTO struct {
+type UserDTO struct {
 	FirstName string `json:"first_name"`
 	LastName  string `json:"last_name"`
 	Email     string `json:"email"`
 	Password  string `json:"password"`
-}
-
-type UpdateUserDTO struct {
-	ID        int     `json:"id"`
-	FirstName *string `json:"first_name"`
-	LastName  *string `json:"last_name"`
-	Email     *string `json:"email"`
-	Password  *string `json:"password"`
 }
